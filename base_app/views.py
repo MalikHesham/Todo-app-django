1f7995f2f71b85be4dbef82d7c21ee5440f08d88
Malik Hesham <malikhesham@outlook.com>
2021-05-02 01:38:05 +0200

    crud completed

--- base_app/views.py
+++ base_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView , DeleteView
 from django.urls import reverse_lazy
 
 
@@ -21,5 +21,14 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = "__all__"
-    template_name = 'base_app/create.html'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'deleted_task'
     success_url = reverse_lazy('tasks')
