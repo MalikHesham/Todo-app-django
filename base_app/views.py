e04cd1ff3dc75061a4c07969790b39f4b0a5cf92
Malik Hesham <malikhesham@outlook.com>
2021-05-01 23:32:11 +0200

    setting the base for class based views

--- base_app/views.py
+++ base_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'base_app/list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base_app/details.html'
+
--- todo_app/settings.py
+++ todo_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base_app.apps.BaseAppConfig'
 ]
 
 MIDDLEWARE = [
